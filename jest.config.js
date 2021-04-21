9a152359195a00e50c4035121d7950346b659c13
Oskar Adin <oskar.adin@evolvetechnology.se>
2021-04-21 15:50:19 +0200

    Ignoring stuff

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  // modulePaths: ["src"],
+  // coverageReporters: ["json", "lcov", "text-summary"],
+  collectCoverageFrom: [
+    "!<rootDir>/**/.next/**",
+    "**/*.{tsx,ts}",
+    "!**/mocks/**",
+    "!**.d.ts",
+    "!**testHelpers.ts",
+  ],
   moduleNameMapper: {
     "\\.(scss|sass|css)$": "identity-obj-proxy",
   },
